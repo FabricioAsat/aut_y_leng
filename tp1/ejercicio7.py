0e4e74d5db8fc3205c28ede5cf609d30eb7fb2b3
Fabricio Asat <fabricioasat00@gmail.com>
2023-08-24 13:10:21 -0300

    ejercicio 7 agregado

---
+++ tp1/ejercicio7.py
@@ -0,0 +1,45 @@
+# Programar una función Python que procese el contenido de un archivo .csv con las columnas
+# [id, first_name, last_name, email, gender, avatar, job_title, language, ssn] y genere un nuevo archivo .csv
+
+# a) -> se cambiará el orden las columnas first_name y last_name y se elimina gender
+#       quedando: [id, last_name, first_name, email, avatar, job_title, language, ssn]
+
+# b) -> y se modificara el dominio en las direcciones de correo, sustituyendo el dominio actual
+#       (parte derecha luego del símbolo @) por @ing.unlpam.edu.ar
+
+import csv
+import re
+
+
+def convert(input_file_path: str, output_file_path: str) -> None:
+    if len(input_file_path) == 0 and len(output_file_path) == 0:
+        print("No se aceptan cadenas vacías")
+        return None
+
+    #
+    with open(input_file_path, "r", newline="", encoding="utf-8") as input_file, \
+            open(output_file_path, "w", newline="", encoding="utf-8") as output_file:
+        reader = csv.reader(input_file)
+        writer = csv.writer(output_file)
+
+        for line_with_info in reader:
+            if "first_name" and "last_name" and "gender" in line_with_info:
+                position_first_name = line_with_info.index("first_name")
+                position_last_name = line_with_info.index("last_name")
+                position_gender = line_with_info.index("gender")
+                position_email = line_with_info.index("email")
+
+            # Acá realizo el ejercicio a)
+            line_with_info[position_first_name], line_with_info[position_last_name] = \
+                line_with_info[position_last_name], line_with_info[position_first_name]
+            del line_with_info[position_gender]
+
+            # Acá realizo el ejercicio b)
+            target = r'@(\w|\.)+$'
+            replace = "@ing.unlpam.edu.ar"
+            line_with_info[position_email] = re.sub(target, replace, line_with_info[position_email])
+
+            writer.writerow(line_with_info)
+
+        input_file.close()
+        output_file.close()
--- tp1/main.py
+++ tp1/main.py
@@ -1,4 +1,5 @@
 from ejercicio6 import *
+from ejercicio7 import *
 
 if __name__ == '__main__':
 
@@ -11,54 +12,4 @@ if __name__ == '__main__':
                  "1.1.1.1:0123"]
     cadenas_g = ["0000 0000 0000 0000", "000 000 000 000", "0000 000 000 000"]
 
-    print("\n\nEjercicio a:")
-    for string in cadenas_a_b:
-        if validate_a(string):
-            print(string, "es un número par")
-        else:
-            print(string, "NO es un número par")
-
-    print("\n\nEjercicio b:")
-
-    for string in cadenas_a_b:
-        if validate_b(string):
-            print(string, "es un número par válido")
-        else:
-            print(string, "NO es un número par válido o tiene ceros innecesarios")
-
-    print("\n\nEjercicio c:")
-    for string in cadenas_c:
-        if validate_c(string):
-            print(string, "es una variable de python posible")
-        else:
-            print(string, "NO puede ser una variable de python")
-
-    print("\n\nEjercicio d:")
-    for string in cadenas_d:
-        if validate_d(string):
-            print(string, "responde a: a^n b c^m")
-        else:
-            print(string, "NO responde a: a^n b c^m")
-
-    print("\n\nEjercicio e:")
-    for string in cadenas_e:
-        if validate_e(string):
-            print(string, "es una dirección ipv4 posible")
-        else:
-            print(string, "NO es una dirección ipv4")
-
-    print("\n\nEjercicio f:")
-
-    for string in cadenas_f:
-        if validate_f(string):
-            print(string, "es una dirección ipv4 con puerto posible")
-        else:
-            print(string, "NO es una dirección con puerto ipv4")
-
-    print("\n\nEjercicio g:")
-
-    for string in cadenas_g:
-        if validate_g(string):
-            print(string, "es un núm válido VISA")
-        else:
-            print(string, "NO es un núm válido VISA")
+    convert("input.csv", "output.csv")
