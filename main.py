05527caba2466bceccefd3b14d20b2b348aae0e7
Fabricio Asat <fabricioasat00@gmail.com>
2023-09-14 18:32:12 -0300

    :sparkles: - tp2 e9 finished

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from tp1.tests.test_e6 import run_tests_tp1_ej6
 from tp1.tests.test_e7 import run_tests_tp1_ej7
+from tp2.ejercicio9 import validate
+
 
 if __name__ == "__main__":
-    run_tests_tp1_ej6()
-    run_tests_tp1_ej7()
+    # run_tests_tp1_ej6()
+    # run_tests_tp1_ej7()
+
+    a = validate("0001111111")
+    print(a)
--- tp2/ejercicio9.py
+++ tp2/ejercicio9.py
@@ -1,30 +1,75 @@
 import json
 
+"""
+Response = {
+        "original_string": string,
+        "modified_string": string,
+        "isValidString": boolean,
+    }
+"""
+
 
 def validate(string: str):
+    automaton = {}
+
     with open("tp2/file.json") as json_f:
         automaton = json.load(json_f)
+        json_f.close()
 
-        todos_los_q = automaton["Q"]
-        todos_los_sigma = automaton["Sigma"]
-        todos_los_gamma = automaton["Gamma"]
-        todos_los_delta = automaton["Delta"]
-        todos_los_b = automaton["B"]
-        todos_los_q0 = automaton["q0"]
-        todos_los_f = automaton["F"]
+    all_symbols = automaton["Gamma"]  # ['0', '1']
+    start = automaton["q0"]  # q0
+    blank = automaton["B"]  # "#"
+    final = automaton["F"]  # ["q3", ...]
+    operations = automaton["Delta"]  # {} todos los q
 
-        print("Q: ", todos_los_q)
-        print("Sigma: ", todos_los_sigma)
-        print("Gamma: ", todos_los_gamma)
-        print("Delta: ", todos_los_delta)
-        print("B: ", todos_los_b)
-        print("Q0: ", todos_los_q0)
-        print("F: ", todos_los_f)
+    state = {"q": start, "position": 0}
+    response = {
+        "original_string": string,
+        "modified_string": "",
+        "isValidString": False,
+    }
 
-        for letter in string:
-            
-            
-            pass
+    # Inserto el "blank" al final de la cadena
+    string_list = list(string + blank)
 
+    # Si hay simbolos en la cadena que no están incluidos en el string dado, automáticamente valido a falso.
+    for letter in set(string):
+        try:
+            all_symbols.index(letter)
+        except:
+            return response
 
-validate("01010101010101")
+    i = 0
+
+    while True:
+        # Determina si el programa llegó a su final "F"
+        for final_symbol in final:
+            if state["q"] == final_symbol:
+                response["isValidString"] = True
+
+        # ------------- Corta cuando ya es final
+        if response["isValidString"] == True:
+            response["modified_string"] = "".join(string_list)
+            break
+        # ---------
+
+        # si 0 está en las operaciones[q0, q1, q2, ...]
+        if string_list[state["position"]] in operations[state["q"]]:
+            next_q = operations[state["q"]][string_list[state["position"]]]["q"]
+
+            if operations[state["q"]][string_list[state["position"]]]["m"] == "r":
+                new_position = state["position"] + 1
+            else:
+                new_position = state["position"] - 1
+
+            string_list[state["position"]] = operations[state["q"]][
+                string_list[state["position"]]
+            ]["e"]
+
+            state["q"] = next_q
+            state["position"] = new_position
+
+        else:
+            return response
+
+    return response
